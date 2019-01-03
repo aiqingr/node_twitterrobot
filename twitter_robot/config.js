9761d8e6bc2f0a937f49fd105d5856f774b2bafc
Ni Tongyang <ntyaiqingr@gmail.com>
2019-01-02 23:02:22 -0500

    remove my api configuration file

--- twitter_robot/config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  consumer_key:         'Srv92NOkJwPjvjqdNlZodS0iI',
-  consumer_secret:      'x2zlW9PppzB3bLJHR0IyPlsp4sVAUSsqt9FMRfyu9YjtphRSFt',
-  access_token:         '1079714202183442432-bveg8wsH5ohbzwsB9KdLRZoe1kxCQK',
-  access_token_secret:  'WFOoOUsojbaBs1LCbhqVClzM4U30t3nkwnq0F7rTJtat4'
-}
