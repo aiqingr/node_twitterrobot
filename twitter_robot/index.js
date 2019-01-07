5076b445dbc8bb9d27c1788c2490b311c355e062
Ni Tongyang <ntyaiqingr@gmail.com>
2019-01-06 22:30:13 -0500

    get result with get method

--- twitter_robot/index.js
+++ twitter_robot/index.js
@@ -3,3 +3,17 @@ console.log("The robot is starting");
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
+const params = {
+  q: 'tongyang',
+  count: 20
+};
+T.get('search/tweets', params, gotData);
+function gotData(err, data, response) {
+  let tweets = data.statuses;
+  for (let i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text)
+  }
+};
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+// });
